3d25f2d8ce984fba97965c1d5ba5a12ca3ed7fe0
Darya Nazarova <nazarova.darya1@yandex.ru>
2025-07-06 14:56:24 +0300

    Арифметические операции_задание

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -1,11 +1,27 @@
 package com.stepup;
 
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+    System.out.println("Введите первое число:");
+    int number_1 = new Scanner(System.in).nextInt();
+    System.out.println("Введите второе число:");
+    int number_2 = new Scanner(System.in).nextInt();
+
+
+    //сумма int
+    int sum=number_1+number_2;
+    System.out.println("Сумма: "+sum);
+    //разность int
+    int difference =number_1-number_2;
+    System.out.println("Разность: "+ difference);
+    //произведение int
+    int proizv=number_1*number_2;
+    System.out.println("Произведение: "+proizv);
+    //частное double
+    double quotient=(double) number_1/number_2;
+    System.out.println("Частное: "+ quotient);
         }
     }
