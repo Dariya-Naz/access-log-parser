6f1e75b26048482355236f63ae88264c854565d9
Darya Nazarova <nazarova.darya1@yandex.ru>
2025-07-22 18:13:24 +0300

    Курсовой проект. Задание №1

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -1,7 +1,26 @@
 package com.stepup;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count=0;
+        while (true) {
+            String path = new Scanner(System.in) .nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if ((fileExists==false) || (isDirectory==true)) {
+                System.out.println("Файл не существует или является директорией");
+                continue;
+            }
+            if (fileExists==true) {
+                System.out.println("Путь указан верно");
+                count+=1;
+                System.out.println("Это файл номер "+count);
+            }
+        }
         }
     }
